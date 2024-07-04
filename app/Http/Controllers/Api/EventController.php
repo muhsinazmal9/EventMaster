5286c0a2a82aa05fe9d421b7715798ccbccd1465
Muhsin Azmal <muhsinstudent12@gmail.com>
2024-07-04 14:51:18 +0600

    feat(events api): add events api controller

---
+++ app/Http/Controllers/Api/EventController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreEventRequest;
+use App\Http\Requests\UpdateEventRequest;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    // ...
+}
